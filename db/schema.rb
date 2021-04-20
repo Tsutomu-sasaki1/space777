cc4ba5f1962e5659817455d8fbcf634938981153
Tsutomu-sasaki1 <tsutomusasaki1209@yahoo.co.jp>
2021-04-20 13:59:59 +0900

    不要なマイグレーション 　削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_09_141133) do
+ActiveRecord::Schema.define(version: 2021_04_09_105352) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
